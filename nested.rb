5f72510625655172d4a306d638ad1a9fb2b843c2
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 21:58:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
 }
     x = :yukihiro_matsumoto => {
   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
+    :languages => ["LISP", "C"]}
     {x}.merge(programmer_hash) 
 end
 
