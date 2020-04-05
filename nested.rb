ef23cee443f0a0834294fc02554624b3bb74c015
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 22:10:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,11 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
 }
-programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
-    languages:
-      ["LISP", "C"]
-end
+programmer_hash[:yukihiro_matsumoto] => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
 
 def changing_alan
 	programmer_hash = 
